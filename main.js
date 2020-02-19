93084873567b8fb18b0a75d7c8e9f61fe7c44d41
KrisCastillo <criscastillo2@hotmail.com>
2020-02-19 08:34:55 -0500

    added html js css md

---
+++ main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const apiKey = 'NqXGXlhMtGwAhrKjQkbxTsgAyLFby0y13Ne8anSf'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+function displayResults(responseJson) {
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++){
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <p><a href=${responseJson.data[i].url}</a>Website</p>
+      </li>`
+    )};
+    $('.results').removeClass('hidden');
+ 
+};
+
+function getNationalParks(query, maxResults=10) {
+  const params = {
+    limit: maxResults,
+    stateCode: query,
+    api_key: apiKey,
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error').text(`Something went wrong, try another. ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNationalParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
